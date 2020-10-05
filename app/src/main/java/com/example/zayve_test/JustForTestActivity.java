d27552c1d339fe18d3eeeabc9e5a77b22821b567
Amir <apahadi73@gmail.com>
2020-10-05 10:49:36 -0500

    saving before editing

--- app/src/main/java/com/example/zayve_test/JustForTestActivity.java
+++ app/src/main/java/com/example/zayve_test/JustForTestActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import com.example.zayve_test.BrowseFriendsFragment;
 
 import android.content.Intent;
 import android.net.Uri;
